bc4f4e6fdc8c57536df2248b20833c5959cebc28
Juan Velázquez <juankapersonalpy@gmail.com>
2025-08-31 14:03:49 -0300

    Configura Whitenoise para servir archivos estáticos

--- Edusec/settings.py
+++ Edusec/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Añade esta línea
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Esto es opcional, pero si tienes una carpeta 'static' a nivel de proyecto, la añades aquí
